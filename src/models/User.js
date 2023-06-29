b56bab151d689a6a9fdc5500bd603376493ca6c4
hamza <benfaida.hamza2014@gmail.com>
2023-06-29 15:03:01 +0200

    fichier auth

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const UsersSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number
+  },
+  address: {
+    type: String
+  },
+  phone: {
+    type: String
+  },
+  role: {
+    type: String
+  }
+}, { versionKey: false });
+
+module.exports = mongoose.model('Users', UsersSchema, 'Users');
+
